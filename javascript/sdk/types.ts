cba2b4abeb38db46a7974764ed2ee1f9eb584c90
al-abd <nexnema@gmail.com>
2024-09-24 17:21:02 +0330

    types

---
+++ javascript/sdk/types.ts
@@ -0,0 +1,19 @@
+import { LangCodeType } from "./src/langCode";
+
+import { MushafListProps, MushafViewProps } from "./src/interfaces/mushaf";
+import { SurahListProps, SurahViewProps } from "./src/interfaces/surah";
+import {
+    TranslationListProps,
+    TranslationViewProps,
+} from "./src/interfaces/translation";
+
+export { type LangCodeType };
+
+export {
+    type MushafListProps,
+    type MushafViewProps,
+    type SurahListProps,
+    type SurahViewProps,
+    type TranslationListProps,
+    type TranslationViewProps,
+};
