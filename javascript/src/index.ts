0ac10924f648528c3f58aea5bc39709e385928f9
ekiuse <0ekiuse1@gmail.com>
2025-01-14 16:34:29 +0330

    Import RequrstBody and params interface

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -4,44 +4,108 @@ export { type LangCodeType };
 
 //Interfaces
 import {
+    AccountSendCoderequestBody,
+    AccountVerifyrequestBody,
+} from "./interfaces/account.js";
+import { UserListResponseData, UserAddrequestBody } from "./interfaces/user.js";
+import {
+    OrganizationListResponseData,
+    OrganizationViewResponseData,
+    OrganizationAddrequestBody,
+    OrganizationNameListResponseData,
+    OrganizationNameViewResponseData,
+    OrganizationNameAddrequestBody,
+    OrganizationNameEditrequestBody,
+} from "./interfaces/organization.js";
+import {
+    PermissionListResponseData,
+    PermissionViewResponseData,
+    PermissionAddrequestBody,
+    PermissionEditrequestBody,
+} from "./interfaces/permission.js";
+import {
     MushafListResponseData,
     MushafViewResponseData,
+    MushafAddrequestBody,
+    MushafListRequestParameters,
 } from "./interfaces/mushaf.js";
 import {
     SurahListResponseData,
     SurahViewResponseData,
+    SurahViewrequestBody,
+    SurahListRequestParameters,
+    SurahViewRequestParameters,
 } from "./interfaces/surah.js";
 import {
     AyahListResponseData,
     AyahViewResponseData,
+    AyahAddrequestBody,
+    AyahListRequestParameters,
 } from "./interfaces/ayah.js";
+import { WordAddrequestBody, WordViewResponseData } from "./interfaces/word.js";
 import {
     TranslationListResponseData,
     TranslationViewResponseData,
+    TranslationAddrequestBody,
+    TranslationListRequestParameters,
+    TranslationViewRequestParameters,
+    TranslationTextModifyrequestBody,
+    TranslationTextViewResponseData,
+    TranslationTextViewRequestParameters,
 } from "./interfaces/translation.js";
 import {
     PhraseListResponseData,
     PhraseViewResponseData,
+    PhraseAddrequestBody,
 } from "./interfaces/phrase.js";
-import { WordViewResponseData } from "./interfaces/word.js";
 import {
-    PermissionListResponseData,
-    PermissionViewResponseData,
-} from "./interfaces/permission.js";
+    ProfileListResponseData,
+    ProfileEditrequestBody,
+} from "./interfaces/profile.js";
 export {
-    type MushafListResponseData,
-    type MushafViewResponseData,
-    type SurahListResponseData,
-    type SurahViewResponseData,
-    type AyahListResponseData,
-    type AyahViewResponseData,
-    type TranslationListResponseData,
-    type TranslationViewResponseData,
-    type PhraseListResponseData,
-    type PhraseViewResponseData,
-    type WordViewResponseData,
+    type AccountSendCoderequestBody,
+    type AccountVerifyrequestBody,
+    type UserListResponseData,
+    type UserAddrequestBody,
+    type OrganizationListResponseData,
+    type OrganizationViewResponseData,
+    type OrganizationAddrequestBody,
+    type OrganizationNameListResponseData,
+    type OrganizationNameViewResponseData,
+    type OrganizationNameAddrequestBody,
+    type OrganizationNameEditrequestBody,
     type PermissionListResponseData,
     type PermissionViewResponseData,
+    type PermissionAddrequestBody,
+    type PermissionEditrequestBody,
+    type MushafListResponseData,
+    type MushafViewResponseData,
+    type MushafAddrequestBody,
+    type MushafListRequestParameters,
+    type SurahListResponseData,
+    type SurahViewResponseData,
+    type SurahViewrequestBody,
+    type SurahListRequestParameters,
+    type SurahViewRequestParameters,
+    type AyahListResponseData,
+    type AyahViewResponseData,
+    type AyahAddrequestBody,
+    type AyahListRequestParameters,
+    type WordAddrequestBody,
+    type WordViewResponseData,
+    type TranslationListResponseData,
+    type TranslationViewResponseData,
+    type TranslationAddrequestBody,
+    type TranslationListRequestParameters,
+    type TranslationViewRequestParameters,
+    type TranslationTextModifyrequestBody,
+    type TranslationTextViewResponseData,
+    type TranslationTextViewRequestParameters,
+    type PhraseListResponseData,
+    type PhraseViewResponseData,
+    type PhraseAddrequestBody,
+    type ProfileListResponseData,
+    type ProfileEditrequestBody,
 };
 
 //Controllers
