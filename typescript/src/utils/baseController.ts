3d9f46d399adbee48d9b6821c86b62c2d5369d88
silence0ne <silenceonew@gmail.com>
2025-04-10 15:12:45 +0330

    Update BaseController methods to return full AxiosResponse

--- typescript/src/utils/baseController.ts
+++ typescript/src/utils/baseController.ts
@@ -1,4 +1,4 @@
-import { AxiosInstance, AxiosRequestConfig } from "axios";
+import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 import { Connection } from "../client/connection";
 import { RequestConfig } from "../utils/globalTypes";
 
@@ -33,28 +33,19 @@ export class BaseController {
     }
 
     // Optionally, you can have methods for making common requests like GET, POST, etc.
-    protected axiosGet<T, P>(
-        url: string,
-        config?: RequestConfig<P>
-    ): Promise<T> {
-        return this.conn.axios
-            .get(url, this.getAuthConfig(config))
-            .then((res) => res.data);
+    protected axiosGet<T, P>(url: string, config?: RequestConfig<P>): Promise<AxiosResponse<T>> {
+        return this.conn.axios.get(url, this.getAuthConfig(config));
     }
 
     protected axiosPost<T>(
         url: string,
         data: any,
         config?: RequestConfig
-    ): Promise<T> {
-        return this.conn.axios
-            .post(url, data, this.getAuthConfig(config))
-            .then((res) => res.data);
+    ): Promise<AxiosResponse<T>> {
+        return this.conn.axios.post(url, data, this.getAuthConfig(config));
     }
 
-    protected axiosDelete<T>(url: string, config?: RequestConfig): Promise<T> {
-        return this.conn.axios
-            .delete(url, this.getAuthConfig(config))
-            .then((res) => res.data);
+    protected axiosDelete<T>(url: string, config?: RequestConfig): Promise<AxiosResponse<T>> {
+        return this.conn.axios.delete(url, this.getAuthConfig(config));
     }
 }
