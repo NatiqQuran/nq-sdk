50f8be94402c25a3ce3ae8e31941f5763d639052
nyzd <abedi.mahdi.dev@gmail.com>
2025-08-06 19:24:10 +0330

    jinja2 added

--- scripts/code-generator/codegen/typescript/codegen.py
+++ scripts/code-generator/codegen/typescript/codegen.py
@@ -1,7 +1,12 @@
 from parser.ast import Ast
+from jinja2 import Template
 
 class Codegen():
     """Typescript code generation
     """
     def __init__(self, ast: Ast):
         self.ast = ast
+        self.template = Template(open("templates/controller.jinja2").read())
+
+    def generate(self) -> str:
+        return self.template.render(ast=self.ast)
